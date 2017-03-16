84b451510075660acbfcc87dd3bdd989cd3fd043
Kevin <kevinj@theodo.fr>
2017-03-17 00:20:58 +0100

    Add receive message route

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,7 +1,9 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 
 var VALIDATION_TOKEN = 'EAADfnKWUqqgBAB2qpOALrtmvxWuiHDsEW8aVUSRdOZChvtG2fpT5coT3GDl0taZBGWgybuHCcu23PqOMsjIzhB8TuATHjGihxb52glDTEO1tZCvSMP2bOccSXFgCkwQdh49aZCTOBSWV8vmIPgGhncjHEkreF6jRMuJaPTUrGQZDZD';
+var PAGE_ACCESS_TOKEN = 'EAADfnKWUqqgBAB2qpOALrtmvxWuiHDsEW8aVUSRdOZChvtG2fpT5coT3GDl0taZBGWgybuHCcu23PqOMsjIzhB8TuATHjGihxb52glDTEO1tZCvSMP2bOccSXFgCkwQdh49aZCTOBSWV8vmIPgGhncjHEkreF6jRMuJaPTUrGQZDZD'
 
 /* GET webhook auth. */
 router.get('/', function(req, res, next) {
@@ -14,5 +16,100 @@ router.get('/', function(req, res, next) {
     res.sendStatus(403);
   }
 });
+
+/* POST webhook */
+router.post('/', function (req, res) {
+  var data = req.body;
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(function(entry) {
+      var pageID = entry.id;
+      var timeOfEvent = entry.time;
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.message) {
+          receivedMessage(event);
+        } else {
+          console.log("Webhook received unknown event: ", event);
+        }
+      });
+    });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know
+    // you've successfully received the callback. Otherwise, the request
+    // will time out and we will keep trying to resend.
+    res.sendStatus(200);
+  }
+});
+
+function receivedMessage(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
+
+  console.log("Received message for user %d and page %d at %d with message:",
+    senderID, recipientID, timeOfMessage);
+  console.log(JSON.stringify(message));
+
+  var messageId = message.mid;
+
+  var messageText = message.text;
+  var messageAttachments = message.attachments;
+
+  if (messageText) {
+    switch (messageText) {
+      case 'generic':
+        sendGenericMessage(senderID);
+        break;
+      default:
+        sendTextMessage(senderID, messageText);
+    }
+  } else if (messageAttachments) {
+    sendTextMessage(senderID, "Message with attachment received");
+  }
+}
+
+function sendGenericMessage(recipientId, messageText) {
+  // To be expanded in later sections
+}
+
+function sendTextMessage(recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText
+    }
+  };
+
+  callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s",
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });
+}
 
 module.exports = router;
