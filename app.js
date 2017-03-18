51c461f7ef547154504d901cc2f4d428466a7fe7
Kevin <kevinj@theodo.fr>
2017-03-19 00:12:55 +0100

    Change route name from geolocalisation to weather

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
+var weather = require('./routes/weather');
 var webhook = require('./routes/webhook');
 
 var app = express();
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/weather', weather);
 app.use('/webhook', webhook);
 
 // catch 404 and forward to error handler
--- routes/weather.js
+++ routes/weather.js
@@ -5,8 +5,8 @@ const
   weatherService = require('../server/weatherService'),
   parser = require('json-parser');
 
-/* GET geolocalisation page. */
-router.get('/geolocalisation', function(req, res, next) {
+/* GET weather page. */
+router.get('/', function(req, res, next) {
   weatherService.getGeolocalisation('Grenoble')
     .then(function (body) {
       res.send(parser.parse(body).results[0].geometry.location);
